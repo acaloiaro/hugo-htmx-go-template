227a915d2b86d3c3f0e7e12c10fa2249269a13b7
Adriano Caloiaro <me@adriano.fyi>
2023-07-24 08:52:02 -0600

    Add mising space between attributes in bin/fetch-deps.sh

--- bin/fetch-deps.sh
+++ bin/fetch-deps.sh
@@ -3,4 +3,4 @@
 HTMX_VERSION=1.9.2
 
 curl -sL "https://unpkg.com/htmx.org@$HTMX_VERSION" > static/htmx.js
-echo '<script type="text/javascript"src="/htmx.js"></script>' > themes/ananke/layouts/partials/site-scripts.html
+echo '<script type="text/javascript" src="/htmx.js"></script>' > themes/ananke/layouts/partials/site-scripts.html
